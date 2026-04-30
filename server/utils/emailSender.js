c2c19c574c2b5b1bce803196296c9532d82010b3
HarshPatel2704 <hp210522@gmail.com>
2026-05-01 00:01:59 +0530

    Update emailSender.js

--- server/utils/emailSender.js
+++ server/utils/emailSender.js
@@ -6,8 +6,8 @@ const sendWelcomeEmail = async (user) => {
 
     const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
