e9c14b1c80f2954455f0f8aa0058a0bf333bc15c
HarshPatel2704 <hp210522@gmail.com>
2026-05-02 23:43:26 +0530

    Update emailSender.js

--- server/utils/emailSender.js
+++ server/utils/emailSender.js
@@ -5,13 +5,15 @@ const sendWelcomeEmail = async (user) => {
     }
 
     const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // Must be false for port 587
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
     tls: {
-        rejectUnauthorized: false 
+        rejectUnauthorized: false // Helps bypass the ENETUNREACH error on Render
     }
 });
 
