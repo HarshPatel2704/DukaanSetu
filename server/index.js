4410f783526c9f162438f800e80d8c0b8798b6df
HarshPatel2704 <hp210522@gmail.com>
2026-05-02 23:03:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ const io = new Server(server, {
     }
 });
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://dukaansetu-nine.vercel.app"],
+    credentials: true
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
